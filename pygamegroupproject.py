1f1a9f6c0b518be12bbb28c816637358fde6a1c6
redhawk-max <146543468+redhawk-max@users.noreply.github.com>
2024-07-31 18:24:05 +0400

    Update pygamegroupproject.py

--- pygamegroupproject.py
+++ pygamegroupproject.py
@@ -7,7 +7,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("enemy Dodger")
+pygame.display.set_caption("Enemy Dodger")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FPS = 60
